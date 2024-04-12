16b8405470fcd568e0d140c05be77d2e2932ad1f
suyundykovv <230639@astanait.edu.kz>
2024-04-12 23:06:50 +0600

    ser

---
+++ src/ads1/ads2.java
@@ -0,0 +1,18 @@
+package ads1;
+
+public class ads2 {
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        int result = factorial(number);
+        System.out.println(number + "! = " + result);
+    }
+}
